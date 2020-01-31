4fbfd5b54949a5a0933205883e79485ffc838db9
Kirill <kirill.davletshin@hypermethod.ru>
2020-01-31 18:53:46 +0300

    продолжение работы с хранилищем

---
+++ client/src/components/mainComponent/modal-view/modalView.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="modal-view" >
+      {{ sss }}
+        <div class="modal-view__actions" @mouseenter="flagActions = true" @mouseleave="flagActions = false">
+          <div class="actions-icon" v-if="!flagActions" >++</div>
+          <div v-if="flagActions" class="modal-view__actions-close" @click="closeModalView">X</div>
+        </div>
+        <div class="modal-view__container">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    const server = require('./../../../config/client/client').server;
+    import Axios from 'axios'
+    export default {
+        /**
+         * компонент общий контайнер для просмотра файлов
+         */
+        name: "modalView",
+        props: {
+            typeFile: {
+                type: String,
+                default: ''
+            },
+            urlFile: {
+                type: String,
+                default: ''
+            }
+        },
+        data() {
+            return {
+                flagActions: false
+            }
+        },
+        computed: {
+          sss() {
+                return this.urlFile
+            }
+        },
+        methods: {
+            // закрытие модалки просмотра
+            closeModalView() {
+                this.$emit('closeModalView')
+            },
+        },
+        mounted() {
+            Axios.get(`${server}fileStorage/open?url=${this.urlFile}`).then(res=> console.log(res)).catch(err=> console.log(err))
+        }
+    }
+</script>
+
+<style lang="less">
+.modal-view {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: rgba(128, 128, 128, 0.09);
+  animation: openModalView .3s ease-in-out forwards;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &__actions {
+    width: 75px;
+    height: 35px;
+    border: 1px solid gray;
+    position: absolute;
+    right: 15px;
+    top: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    > div {
+      opacity: 0;
+      transform: translateX(-50px);
+      animation: activeSettings .2s ease-in-out forwards;
+    }
+
+  }
+  &__container {
+    width: 80%;
+    height: 80%;
+    background: rgba(30, 30, 30, 0.46);
+  }
+
+  @keyframes openModalView {
+    0% {
+      opacity: 0.3;
+      transform: scale(.1);
+    }
+    100% {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+  @keyframes activeSettings {
+    0% {
+      opacity: 0;
+      transform: translateX(-50px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
+}
+</style>
